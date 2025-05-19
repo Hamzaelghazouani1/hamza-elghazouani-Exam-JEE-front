0c8bd0b29d07854454c982eec989c6c553fca444
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-05-19 11:44:24 +0100

    feat add http client prov

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Client } from '../../core/models/client';
+import { HttpService } from '../../core/services/http.service';
 
 @Component({
   selector: 'app-client',
@@ -12,26 +13,34 @@ import { Client } from '../../core/models/client';
 })
 export class ClientComponent implements OnInit {
   clients: Client[] = [];
+  http = inject(HttpService);
   newClient: Client = {
     nom: '',
     email: ''
   };
+
   editing: boolean = false;
   selectedClient: Client | null = null;
 
   ngOnInit() {
-    // TODO: Load clients from service
+    this.http.get<Client[]>('clients').subscribe({
+      next: (data) => {
+        this.clients = data;
+        console.log('Clients fetched successfully:', data);
+      },
+      error: (error) => {
+        console.error('Error fetching clients:', error);
+      }
+    })
   }
 
   onSubmit() {
     if (this.editing && this.selectedClient) {
-      // Update existing client
       const index = this.clients.findIndex(c => c.id === this.selectedClient?.id);
       if (index !== -1) {
         this.clients[index] = { ...this.selectedClient, ...this.newClient };
       }
     } else {
-      // Add new client
       this.clients.push({ ...this.newClient, id: Date.now() });
     }
     this.resetForm();
